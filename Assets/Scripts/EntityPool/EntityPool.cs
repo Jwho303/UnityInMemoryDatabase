ecc0fc68405b9b8a28fa9df77f27c9abc0f9b09a
Jeff <jeff@renderheads.com>
2022-07-05 20:41:28 +0200

    Added Entity pool

---
+++ Assets/Scripts/EntityPool/EntityPool.cs
@@ -0,0 +1,83 @@
+//RenderHeads - Jeff Rusch
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InMemoryDatabase;
+using System;
+
+namespace RenderHeads
+{
+    public abstract class EntityPool<T> : MonoBehaviour where T : TableEntry
+    {
+        #region Public Properties
+        public GameEntity<T> Prefab;
+        #endregion
+
+        #region Private Properties
+        private List<GameEntity<T>> _gameEntities = new List<GameEntity<T>>();
+        private Action<GameEntity<T>> _onCreateEntity = delegate { };
+        private Action<GameEntity<T>> _onGetEntity = delegate { };
+        private Action<GameEntity<T>> _onReleaseEntity = delegate { };
+        private int _maxSize;
+
+		#endregion
+
+		#region Public Methods
+		public void Initialize(Action<GameEntity<T>> onCreateEntity, Action<GameEntity<T>> onGetEntity, Action<GameEntity<T>> onReleaseEntity, int maxSize)
+		{
+			_onCreateEntity = onCreateEntity;
+			_onGetEntity = onGetEntity;
+			_onReleaseEntity = onReleaseEntity;
+			_maxSize = maxSize;
+
+			for (int i = 0; i < _maxSize; i++)
+			{
+                CreateEntity();
+			}
+		}
+
+		public GameEntity<T> Get()
+        {
+            GameEntity<T> gameEntity = null;
+
+            for (int i = 0; i < _gameEntities.Count; i++)
+            {
+                if (!_gameEntities[i].gameObject.activeInHierarchy)
+                {
+                    gameEntity = _gameEntities[i];
+                    break;
+                }
+            }
+
+            if (gameEntity == null)
+			{
+                gameEntity = CreateEntity();
+			}
+
+            _onGetEntity(gameEntity);
+
+            return gameEntity;
+        }
+
+        public void Release(GameEntity<T> gameEntity)
+        {
+            _onReleaseEntity(gameEntity);
+        }
+
+        #endregion
+
+        #region Private Methods
+        private GameEntity<T> CreateEntity()
+		{
+            GameEntity<T> gameEntity = Instantiate(Prefab, this.transform);
+            _gameEntities.Add(gameEntity);
+
+            _onCreateEntity(gameEntity);
+
+            return gameEntity;
+        }
+
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/EntityPool/GameEntity.cs
@@ -0,0 +1,32 @@
+//RenderHeads - Jeff Rusch
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InMemoryDatabase;
+using System;
+
+namespace RenderHeads
+{
+    public abstract class GameEntity<T> : MonoBehaviour where T : TableEntry
+    {
+        #region Public Properties
+        public T TableEntry;
+        public Guid Id => TableEntry.Id;
+        #endregion
+
+        #region Private Properties
+
+        #endregion
+
+        #region Public Methods
+        public void Initialize(T tableEntry)
+        {
+            TableEntry = tableEntry;
+		}
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
